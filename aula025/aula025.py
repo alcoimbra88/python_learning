86e2fcc1df2382c821de16d6f50d0afd4a48ac15
Alan Coimbra <al_coimbra@hotmail.com>
2022-08-09 01:09:23 -0300

    Segundo Commit

---
+++ aula025/aula025.py
@@ -0,0 +1,14 @@
+'''
+contador_pai = 0
+contador_filho_1 = 0
+contador_filho_2= 10
+while contador_pai <= 8:
+    print(contador_filho_1, contador_filho_2)
+    contador_filho_1 = contador_filho_1 + 1
+    contador_filho_2 = contador_filho_2 - 1
+    contador_pai = contador_pai +1
+'''
+
+for p, r in enumerate(range(10,1, -1)):
+    print(p,r)
+
