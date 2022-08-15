f96783cdf290f69feb48b3a0b3ff498eb286fde7
Alan <al_coimbra@hotmail.com>
2022-08-15 20:32:32 -0300

    Aulas 29 e 30

---
+++ aula029/aula029.py
@@ -0,0 +1,31 @@
+'''
+    Funçoes (def) em Python - return - Aula 29 (parte 2)
+'''
+
+'''
+def funcao(var):
+    return var
+
+variavel = funcao('Qualquer valor')
+
+if variavel:
+    print(variavel)
+else:
+    print('Vazio')
+'''
+
+
+def divisao(n1, n2):
+    if n2 == 0:
+        return
+    else:
+        return n1/n2
+
+divide = divisao(10, 5)
+
+if divide:
+    print(divide)
+else:
+    print('Conta inválida')
+
+
---
+++ aula030/aula030.py
@@ -0,0 +1,46 @@
+'''
+    1 - Crie uma função que exibe uma saudação com os parâmetros saudação e nome
+
+    2 - Crie uma função que recebe 3 números como parâmetros e exiba a soma entre eles.
+
+    3 - Crie uma função que receba 2 números. O primeiro é uma valor e o segundo um percentual (ex: 10%).
+        Retorne (return) o valor do primeiro número somado do aumento do percentual do mesmo.
+
+    4 - Fizz Buzz - Se o parâmetro da função for divisivel por 2, retorne fizz, se o parâmetro da função for divisivel
+        por 5 retorne buzz. Se o parâmetro da função for divisivel por 5 e por 3 retorna FizzBuzz, caso contrário,
+        retorne o número enviado
+'''
+
+#1
+def saudacao(saud, nome):
+    print(saud, nome)
+
+saudacao('Ola','Alan')
+
+#2
+def soma(n1, n2, n3):
+    print(n1+n2+n3)
+
+soma(2,3,5)
+
+#3
+def calculaPerc (numero,perc):
+    perc = perc / 100
+    diferenca = numero * perc
+    print(numero + diferenca)
+
+calculaPerc(100,10)
+
+#4
+def FizzBuzz(numero):
+    if (numero % 3 == 0) and (numero % 5 == 0):
+        print('FizzBuzz')
+    else:
+        if numero % 3 == 0:
+            print('Fizz')
+        elif numero % 5 == 0:
+            print('Buzz')
+        else:
+            print(numero)
+
+FizzBuzz(15)
