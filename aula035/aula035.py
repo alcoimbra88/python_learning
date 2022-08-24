c7f92ef07e27a364a9b480f7e3ff0e82d80ed023
Alan <al_coimbra@hotmail.com>
2022-08-24 00:43:12 -0300

    Aula 35

---
+++ aula035/aula035.py
@@ -0,0 +1,13 @@
+'''
+    Diferença de tupla para lista
+        Tuplas não é possivel editar os elementos
+'''
+
+t1 = (1,2,3, 'Alan','Coimbra')
+# ou t1 = 1,2,3, 'Alan','Coimbra'
+# para  tupla de 1 item usar virgula ->  t1 = 1,
+
+print(t1[4])
+
+for v in t1:
+    print(v)
